2ea9aaa97aca58a70498f46834a9de16b7bfaba4
tigermumu <34047105+tigermumu@users.noreply.github.com>
2020-10-04 16:47:35 +0800

    Update IFlashLoanReceiver.sol

--- src/aave/contracts/IFlashLoanReceiver.sol
+++ src/aave/contracts/IFlashLoanReceiver.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
